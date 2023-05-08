b91808c879bff59f5509eecb9b33200a097597c3
RGGG1 <>
2023-05-08 17:46:56 +0000

    getting there with the winner algo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 
-const character1 = [10,25];
-const character2 = [10,25];
+let character1 = {attack:75, defence:"25"};
+let character2 = {attack:90, defence:"25"};
 
 function fight() 
 { 
-    
-        document.getElementById("result").innerHTML = character1[1] / character2[0];
+        var random = Math.floor(Math.random() * 100) + 1;
+        console.log(random);
+        var win_chance = character1.attack / (character1.attack + character2.attack);
+        
+        if (random <= win_chance) {
+            document.getElementById("result").innerHTML = "P1";
+          } else {
+            document.getElementById("result").innerHTML = "P2";
+          }
 }
+
