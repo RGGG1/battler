64d5817057d307eb732ac70849fb97d39fad2e17
RGGG1 <>
2023-05-09 16:21:18 +0000

    layout

---
+++ assets/js/script3.js
@@ -0,0 +1,11 @@
+
+    function fight() 
+{ 
+        let language = document.getElementById('language').value;
+        console.log(language);
+        let sport = document.getElementById('sport');
+        console.log(sport);
+        let result = document.getElementById("result");
+        
+        result.innerHTML = "P1";
+          }
