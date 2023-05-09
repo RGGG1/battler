5bb9d2fc7def5af70e1794e32953239ed95c1658
RGGG1 <>
2023-05-09 17:41:52 +0000

    layout

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,13 +1,43 @@
 
-var characterField = document.getElementById('character');
-var rad = document.myForm.myRadios;
-var prev = null;
-for(var i = 0; i < rad.length; i++) {
-    rad[i].onclick = function() {
+let characterField = document.getElementById('character');
+let weaponField = document.getElementById('weapon');
+let specialField = document.getElementById('special');
+let char = document.myForm.characters;
+let weap = document.myForm.weapons;
+let spec = document.myForm.specials;
+let prev = null;
+for(let i = 0; i < char.length; i++) {
+    char[i].onclick = function() {
         console.log(this.value);
         if(this !== prev) {
             prev = this;
             characterField.textContent = this.value;
         }
+    }
+        for(let i = 0; i < weap.length; i++) {
+            weap[i].onclick = function() {
+                console.log(this.value);
+                if(this !== prev) {
+                    prev = this;
+                    weaponField.textContent = this.value;
+                }
+            };
+
     };
+
+    for(let i = 0; i < spec.length; i++) {
+        spec[i].onclick = function() {
+            console.log(this.value);
+            if(this !== prev) {
+                prev = this;
+                specialField.textContent = this.value;
+            }
+        };
+
+};
+    
+
 }
+
+
+
