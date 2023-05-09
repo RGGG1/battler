12979ba7bde4fc5492493ecb30c859361d4ae9ff
RGGG1 <>
2023-05-09 19:08:26 +0000

    setting global variables

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,36 +1,46 @@
 
-let characterField = document.getElementById('character');
-let weaponField = document.getElementById('weapon');
-let specialField = document.getElementById('special');
+// Character traits
+var C1 = {attack:75, defence:"25"};
+var C2 = {attack:90, defence:"25"};
+
+
+// Radio buttons
+let character = document.getElementById('character');
+let weapon = document.getElementById('weapon');
+let special = document.getElementById('special');
 let char = document.myForm.characters;
 let weap = document.myForm.weapons;
 let spec = document.myForm.specials;
-let prev = null;
+var prev = null;
+
+// Radio buttons - character selection
 for(let i = 0; i < char.length; i++) {
     char[i].onclick = function() {
         console.log(this.value);
         if(this !== prev) {
             prev = this;
-            characterField.textContent = this.value;
+            character.textContent = this.value;
+
         }
     }
+// Radio buttons - weapon selection
         for(let i = 0; i < weap.length; i++) {
             weap[i].onclick = function() {
                 console.log(this.value);
                 if(this !== prev) {
                     prev = this;
-                    weaponField.textContent = this.value;
+                    weapon.textContent = this.value;
                 }
             };
 
     };
-
+// Radio buttons - specials selection
     for(let i = 0; i < spec.length; i++) {
         spec[i].onclick = function() {
             console.log(this.value);
             if(this !== prev) {
                 prev = this;
-                specialField.textContent = this.value;
+                special.textContent = this.value;                
             }
         };
 
@@ -39,5 +49,21 @@ for(let i = 0; i < char.length; i++) {
 
 }
 
+// Fight
+
+function fight() 
+{ 
+    let random = Math.floor(Math.random() * 100) + 1;
+        console.log(random);
+        console.log(C1.attack)
+        let win_chance = (character.attack / (character.attack + C2.attack))*100;
+        let result = document.getElementById("result");
+        
+        if (random <= win_chance) {
+            result.innerHTML = "C2";
+          } else {
+            result.innerHTML = "C1";
+          }
+}
 
 
