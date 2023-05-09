3a1d9cab686202e52ae9d84649930508f3769fc8
RGGG1 <>
2023-05-09 16:36:42 +0000

    layout

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,11 +1,13 @@
 
-    function fight() 
-{ 
-        let language = document.getElementById('language').value;
-        console.log(language);
-        let sport = document.getElementById('sport');
-        console.log(sport);
-        let result = document.getElementById("result");
-        
-        result.innerHTML = "P1";
-          }
+var characterField = document.getElementById('character');
+var rad = document.myForm.myRadios;
+var prev = null;
+for(var i = 0; i < rad.length; i++) {
+    rad[i].onclick = function() {
+        console.log(this.value);
+        if(this !== prev) {
+            prev = this;
+            characterField.textContent = this.value;
+        }
+    };
+}
